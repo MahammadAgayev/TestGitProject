d917e95a2edcd6d8c98b98dd0442a73cf25b9362
MahammadAgayev <53854177+MahammadAgayev@users.noreply.github.com>
2020-11-28 14:27:23 +0400

    Update Program.cs

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -10,7 +10,7 @@ namespace TestGitProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
